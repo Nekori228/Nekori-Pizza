3f76da39b5d9dce3d63b31b711b6a985ad41d652
Пользователь <Kalichak_eo@mail.ru>
2024-07-29 17:42:58 +0500

    first commit

---
+++ components/shared/index.ts
@@ -0,0 +1,9 @@
+import { Container } from "./container";
+import { Header } from "./header";
+import { Title } from "./title";
+export { Title, Container };
+import { Categories } from "./categories";
+import { SortPopup } from "./sort-popup";
+import { Filters } from "./filters";
+import { FilterCheckbox } from "./filter-checkboc";
+
