0acce0e8a4813e1f6a1df04e45bc815a16caa9ff
Nekori228 <88834448+Nekori228@users.noreply.github.com>
2024-08-14 10:38:54 +0300

    Update category produc

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,27 @@
 import { Container, Title } from "@/components/shared";
-import { Categories } from "@/components/shared/categories";
 import { Filters } from "@/components/shared/filters";
-import { ProductCard } from "@/components/shared/product-card";
 import { ProductGroupList } from "@/components/shared/products-group-list";
-import { SortPopup } from "@/components/shared/sort-popup";
 import { TopBar } from "@/components/shared/top-bar";
+import { prisma } from "@/prisma/prisma-client";
 
 //7:24:40
-export default function Home() {
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingredients: true,
+          items: true
+        }
+      }
+    }
+  });
+
   return <>
     <Container className="mt-10">
       <Title text="Все пиццы" size="lg" className="font-extrabold"/>
     </Container>
-    <TopBar />
+    <TopBar categories={categories.filter((category) => category.products.length > 0)}/>
 
     <Container className="mt-10 pb-14">
       <div className="flex gap-[80px]">
@@ -24,95 +33,18 @@ export default function Home() {
         {/* Список товаров */}
         <div className="flex-1">
           <div className="flex flex-col gap-16">
-            <ProductGroupList title={"Пиццы"} items={[
-              {
-                id: 1,
-                name: 'Маргаритта',
-                imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif',
-                print: 550,
-                items: [{ price: 550}],
-              },
-              {
-                id: 2,
-                name: 'Маргаритта',
-                imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif',
-                print: 550,
-                items: [{ price: 550}],
-              },
-              {
-                id: 3,
-                name: 'Маргаритта',
-                imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif',
-                print: 550,
-                items: [{ price: 550}],
-              },
-              {
-                id: 4,
-                name: 'Маргаритта',
-                imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif',
-                print: 550,
-                items: [{ price: 550}],
-              },
-              {
-                id: 5,
-                name: 'Маргаритта',
-                imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif',
-                print: 550,
-                items: [{ price: 550}],
-              },
-              {
-                id: 6,
-                name: 'Маргаритта',
-                imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif',
-                print: 550,
-                items: [{ price: 550}],
-              },
-            ]} categoryId={1} />
-
-<ProductGroupList title={"Комбо"} items={[
-              {
-                id: 7,
-                name: 'Маргаритта',
-                imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif',
-                print: 550,
-                items: [{ price: 550}],
-              },
-              {
-                id: 8,
-                name: 'Маргаритта',
-                imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif',
-                print: 550,
-                items: [{ price: 550}],
-              },
-              {
-                id: 9,
-                name: 'Маргаритта',
-                imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif',
-                print: 550,
-                items: [{ price: 550}],
-              },
-              {
-                id: 10,
-                name: 'Маргаритта',
-                imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif',
-                print: 550,
-                items: [{ price: 550}],
-              },
-              {
-                id: 11,
-                name: 'Маргаритта',
-                imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif',
-                print: 550,
-                items: [{ price: 550}],
-              },
-              {
-                id: 12,
-                name: 'Маргаритта',
-                imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D6175C10773BFE36E56D48DF7E3.avif',
-                print: 550,
-                items: [{ price: 550}],
-              },
-            ]} categoryId={2} />
+            {
+              categories.map((category) => (
+                category.products.length > 0 && (
+                  <ProductGroupList 
+                  key={category.id}
+                  title={category.name}
+                  categoryId={category.id}
+                  items={category.products}
+                  />
+                )
+              ))
+            }
           </div>
         </div>
       </div>
